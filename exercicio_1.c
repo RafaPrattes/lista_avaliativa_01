72fe04e9d96422ac3fc5fdf84901891b06064270
Rafaela <142021465+RafaPrattes@users.noreply.github.com>
2023-09-28 20:40:20 -0300

    exercicio_1.c

---
+++ exercicio_1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+int X;
+
+scanf ("%d", &X);
+if(X==1916||X==1940||X==1942||X==1944||X==1946)
+
+{
+printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d. \n ", X);
+   }  
+   
+else{
+if(X % 4 == 0 && X >= 1896 && X!=1916 && X!=1940 && X!=1942 && X!=1944 && X!=1946){
+printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d. \n",X);
+}
+
+if(X % 4 == 2 && X >= 1930 && X!=1916 && X!=1940 && X!=1942 && X!=1944 && X!=1946){
+    printf("A Copa do Mundo de Futebol ocorreu no ano de %d. \n",X);
+}
+
+    else {
+        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d. \n ", X);
+    }
+
+
+} 
+return 0;
+}
